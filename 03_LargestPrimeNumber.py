c801f50bd6d01b1165b71ae4d91cc02575730b80
fpl <francescopaololezza@gmail.com>
2021-12-26 23:14:01 +0100

    ADDED comment on 03 problem

--- 03_LargestPrimeNumber.py
+++ 03_LargestPrimeNumber.py
@@ -3,6 +3,9 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
+To verify:
+    https://www.calculatorsoup.com/calculators/math/prime-factors.php
+
 '''
 
 # In simple words, prime factor is finding which 
@@ -11,12 +14,20 @@ What is the largest prime factor of the number 600851475143 ?
 # Remember that: the largest prime factor 
 # will never be larger than the square root of n
 
-n = 840 #600851475143
+import time
+
+print('Start!')
+start_time = time.time()
+
+nrStart = n = 600851475143
 pf = 2
 
+
+# pf < sqrt(n) --> pf^2 < n --> pf * pf  < n
 while pf * pf < n:
     while n % pf == 0:
         n = n / pf
     pf = pf + 1
 
-print (n)
+print(f'The largest prime factor of {nrStart} is {n}')
+print("--- %s seconds ---" % (time.time() - start_time))
