c0672e1f07020da0c15d4e93faba30f1fdfcbb6b
fpl <francescopaololezza@gmail.com>
2021-12-28 23:03:26 +0100

    ADDED Smallest Multiple problem

---
+++ 05_SmallestMultiple.py
@@ -0,0 +1,40 @@
+'''
+2520 is the smallest number that can be divided
+by each of the numbers from 1 to 10 without any
+remainder.
+
+What is the smallest positive number that is
+evenly divisible by all of the numbers from
+1 to 20?
+
+---
+2520 / 1 = 2520
+2520 / 2 = 1260
+...
+2520 / 7 = 360
+...
+2520 / 10 = 252
+
+'''
+import time
+
+
+def chkMod(nummber):
+    for item in range(1, 21):
+        if (x % item) != 0:
+            return False
+    return True
+
+
+print('Start!')
+start_time = time.time()
+
+x = 0
+
+while True:
+    x += 20
+    if chkMod(x):
+        break
+
+print(f'The smallest positive number is {x}')
+print("--- %s seconds ---" % (time.time() - start_time))
