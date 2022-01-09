0ed84e6784fa4fbe6e05b6e2a199f73a0c9969c4
fpl <francescopaololezza@gmail.com>
2022-01-09 23:36:53 +0100

    ADDED custom problem (missing one number)

---
+++ 999_01_MissingNumber.py
@@ -0,0 +1,43 @@
+'''
+Given an array 'nums' containing 'n' distinct
+numbers in the range [0, n], return the only
+number in the range that is missing from the
+array
+
+Example
+
+	Input: nums = [3, 0, 1]
+	Output: 2
+	Explanation:
+		n = 3 since there are 3 numbers, 
+		so all numbers are in the range [0,3].
+		'2' is the missing number in the range
+		since it does not appear in nums.
+
+'''
+import random
+
+
+def missingNr(maxNr, numbers):
+	complete = sum(range(0, maxNr + 1))
+	partial = sum(numbers)
+	return complete - partial
+
+def verify(n, nums):
+	print(nums)
+	print(f'Missing value is: {missingNr(n, nums)}')
+
+
+n = 3
+nums =  [0, 3, 1]
+verify(n, nums)
+
+n = 9
+nums =  [9,6,4,2,3,5,7,0,1]
+verify(n, nums)
+
+n = random.randint(3, 100)
+nums = list(range(1,n+1))
+nums.pop(random.randint(1,n-1))
+
+verify(n, nums)
