a43aa63279108c7289c616342ad31dfda60d9723
fpl <francescopaololezza@gmail.com>
2022-01-14 21:42:17 +0100

    MODIFIED logic by using no built-in functions (Gauss sum formula!)

--- 999_01_MissingNumber.py
+++ 999_01_MissingNumber.py
@@ -14,13 +14,27 @@ Example
 		'2' is the missing number in the range
 		since it does not appear in nums.
 
+Sum of natural numbers 1..N = N(N+1)2
 '''
 import random
 
+def seriesSumGauss(n):
+	return int(n*(n + 1) / 2)
 
-def missingNr(maxNr, numbers):
-	complete = sum(range(0, maxNr + 1))
-	partial = sum(numbers)
+# def missingNr(maxNr, numbers):
+# 	complete = sum(range(0, maxNr + 1))
+# 	partial = sum(numbers)
+# 	return complete - partial
+
+# def verify(n, nums):
+# 	print(nums)
+# 	print(f'Missing value is: {missingNr(n, nums)}')
+
+def missingNr(maxNr, lstNumbers):
+	partial = 0
+	complete = seriesSumGauss(maxNr)
+	for item in lstNumbers:
+		partial += item
 	return complete - partial
 
 def verify(n, nums):
